a417fa552a4008b4816636cba6f41a503ec6cbdf
yashraj161998 <146554210+yashraj161998@users.noreply.github.com>
2023-09-30 19:42:37 +0530

    Add files via upload

---
+++ Multiplayer Game connect 4.py
@@ -0,0 +1,65 @@
+import numpy as 
+def create_board():
+    board = np.zeros((6, 7))
+    return board
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+def is_valid_move(board, col):
+    return board[5][col] == 0
+
+def get_next_open_row(board, col):
+    for r in range(6):
+        if board[r][col] == 0:
+            return r
+
+
+def winning_move(board, piece):
+    for c in range(4):
+        for r in range(6):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    for c in range(7):
+        for r in range(3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+
+    for c in range(4):
+        for r in range(3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+
+    for c in range(4):
+        for r in range(3, 6):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+
+board = create_board()
+game_over = False
+turn = 0
+
+while not game_over:
+    if turn == 0:
+        col = int(input("Player 1, make your selection (0-6): "))
+        if is_valid_move(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
+            if winning_move(board, 1):
+                print("Player 1 wins!")
+                game_over = True
+
+    else:
+        col = int(input("Player 2, make your selection (0-6): "))
+        if is_valid_move(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
+            if winning_move(board, 2):
+                print("Player 2 wins!")
+                game_over = True
+
+    turn += 1
+    turn %= 2
+
+    print(board)
